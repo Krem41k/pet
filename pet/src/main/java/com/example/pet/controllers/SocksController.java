0f273995a4b6c33c9cce9072131b674dddc9ce21
Daniil Kremyansky <densca@mail.ru>
2023-02-05 14:51:58 +0700

    Добавлен docker-compose и вывод HTTP-кода

--- pet/src/main/java/com/example/pet/controllers/SocksController.java
+++ pet/src/main/java/com/example/pet/controllers/SocksController.java
@@ -4,65 +4,70 @@ import com.example.pet.dao.entity.Socks;
 import com.example.pet.dto.SocksDto;
 import com.example.pet.mapper.SocksMapper;
 import com.example.pet.service.SocksService;
-import com.example.pet.utils.Operation;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class SocksController {
     private final SocksService service;
     private final SocksMapper mapper;
 
     @GetMapping("/socks")
-    public Integer findAll(@RequestParam("color") String color,
+    public ResponseEntity<String> findAll(@RequestParam("color") String color,
                                   @RequestParam("operation") String operation,
                                   @RequestParam("cottonPart") Integer cottonPart) {
         List<Socks> socks = switch (operation) {
             case "lessThan"-> service.findAllByColorAndCottonPartLessThan(color, cottonPart);
             case "moreThan" -> service.findAllByColorAndCottonPartGreaterThan(color, cottonPart);
             case "equal" -> service.findAllByColorAndCottonPartEquals(color, cottonPart);
-            default -> throw new RuntimeException();
+            default -> new ArrayList<>();
         };
-
-        return socks.stream()
+        int sum = socks.stream()
                 .map(mapper::toDto)
                 .mapToInt(SocksDto::getQuantity)
                 .sum();
-    }
-
-    @GetMapping("/socks/{id}")
-    public ResponseEntity<SocksDto> findByID(@PathVariable Long id) {
-        return ResponseEntity.of(service.findById(id).map(mapper::toDto));
-    }
-
-    @PostMapping("/socks")
-    public SocksDto create(@Valid @RequestBody SocksDto socksDto) {
-        Socks socks = service.save(mapper.toEntity(socksDto));
-        return mapper.toDto(socks);
+        if (sum > 0)
+            return ResponseEntity.status(HttpStatus.OK).body(String.valueOf(sum));
+        else
+            return ResponseEntity.badRequest().body("Неверные параметры запроса");
     }
 
     @PostMapping("/socks/income")
-    public SocksDto income(@Valid @RequestBody SocksDto socksDto) {
+    public ResponseEntity<String> income(@Valid @RequestBody SocksDto socksDto) {
         Socks socksIncome = mapper.toEntity(socksDto);
         Socks oldSocks = service.findByColorAndCottonPart(socksIncome.getColor(), socksIncome.getCottonPart());
-        oldSocks.setQuantity(oldSocks.getQuantity() + socksIncome.getQuantity());
-        return mapper.toDto(service.save(oldSocks));
+        if (oldSocks == null){
+            oldSocks = socksIncome;
+        } else {
+            oldSocks.setQuantity(oldSocks.getQuantity() + socksIncome.getQuantity());
+        }
+        mapper.toDto(service.save(oldSocks));
+        return ResponseEntity.status(HttpStatus.OK).body("Удалось добавить приход");
     }
 
     @PostMapping("/socks/outcome")
-    public SocksDto outcome(@Valid @RequestBody SocksDto socksDto) {
+    public ResponseEntity<String> outcome(@Valid @RequestBody SocksDto socksDto) {
         Socks socksIncome = mapper.toEntity(socksDto);
         Socks oldSocks = service.findByColorAndCottonPart(socksIncome.getColor(), socksIncome.getCottonPart());
-        oldSocks.setQuantity(oldSocks.getQuantity() - socksIncome.getQuantity());
-        return mapper.toDto(service.save(oldSocks));
+        if (oldSocks == null){
+            oldSocks = socksIncome;
+        } else {
+            int diff = oldSocks.getQuantity() - socksIncome.getQuantity();
+            if (diff > 0)
+                oldSocks.setQuantity(diff);
+            else
+                return ResponseEntity.badRequest()
+                        .body("Количество носков запрашиваемых на отпуск больше, чем хранится на складе");
+        }
+        mapper.toDto(service.save(oldSocks));
+        return ResponseEntity.status(HttpStatus.OK).body("Удалось зарегистрировать отпуск");
     }
 }
